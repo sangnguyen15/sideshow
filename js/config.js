08b16635155ec455dcb157d180540880e36c96b2
sangnguyen15 <nguyenvasa.0993@gmail.com>
2026-09-05 13:32:22 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  */
 var APP_CONFIG = {
   // ===== DÁN LINK GAS VÀO ĐÂY (sau khi Deploy Web App) =====
-  GAS_URL: 'https://script.google.com/macros/s/DÁN_URL_GAS_VÀO_ĐÂY/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycby9ufJwbzzmhLHkdRLIhjyCfjPH9NyDqKdiZCyHvTMgFPDTkzyzpNGveI5h2vvBc27P/exec',
   // ========================================================
 
   // Timeout khi gọi GAS (ms)
